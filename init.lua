14b0e0218e09b72710f0662585b544e610b76d5c
millnitzluan <luanmillnitz@gmail.com>
2022-04-18 16:13:04 -0300

    update

--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
 vim.opt.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
+vim.o.background = "dark"
 vim.cmd([[colorscheme gruvboy]])
 
-vim.o.fileencoding = "utf-8" -- File content encoding for the buffer
+vim.o.fileencoding = "utf-8"
 vim.o.syntax = "on"
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.ruler = true
 vim.o.mouse = "a"
 vim.o.clipboard = "unnamedplus"
-vim.o.smartindent = true -- Do auto indenting when starting a new line
+vim.o.smartindent = true
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-vim.o.expandtab = true -- Enable the use of space in tab
+vim.o.expandtab = true
 
-vim.o.swapfile = false -- Disable use of swapfile for the buffer
+vim.o.swapfile = false
 
-vim.o.scrolloff = 10 -- Number of lines to keep above and below the cursor
-vim.o.sidescrolloff = 10 -- Number of columns to keep at the sides of the cursor
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 10
 
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
