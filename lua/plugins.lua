7cfd8491615674585ff28c09521db3f612a43f62
millnitzluan <luanmillnitz@gmail.com>
2022-05-05 11:44:39 -0300

    move gitsigns config to plugins/gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,35 +56,7 @@ local startup = function(use)
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			require("gitsigns").setup({
-				on_attach = function(bufnr)
-					local gs = package.loaded.gitsigns
-
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
-					end
-
-					-- Actions
-					-- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-					-- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-					-- map('n', '<leader>hS', gs.stage_buffer)
-					-- map('n', '<leader>hu', gs.undo_stage_hunk)
-					-- map('n', '<leader>hR', gs.reset_buffer)
-					-- map('n', '<leader>hp', gs.preview_hunk)
-					map("n", "<leader>hb", function()
-						gs.blame_line({ full = true })
-					end)
-					-- map('n', '<leader>tb', gs.toggle_current_line_blame)
-					-- map('n', '<leader>hd', gs.diffthis)
-					-- map('n', '<leader>hD', function() gs.diffthis('~') end)
-					-- map('n', '<leader>td', gs.toggle_deleted)
-					--
-					-- Text object
-					-- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-				end,
-			})
+			require("plugins.gitsigns")
 		end,
 	})
 
@@ -95,7 +67,6 @@ local startup = function(use)
 			require("luasnip/loaders/from_vscode").lazy_load()
 		end,
 		requires = {
-			-- Snippet collections
 			"rafamadriz/friendly-snippets",
 		},
 	})
