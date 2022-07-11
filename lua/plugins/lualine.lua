94ed23aa676e674bb44724212f03c1be048c1a18
millnitzluan <luanmillnitz@gmail.com>
2022-07-11 14:39:29 -0300

    disable lualine in nerdtree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -34,6 +34,7 @@ require("lualine").setup({
   options = {
     component_separators = "",
     section_separators = { left = "", right = "" },
+    disabled_filetypes = { "NvimTree" },
   },
   sections = {
     lualine_a = {
