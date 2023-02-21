dc1d1b6a4cac714795b390c0bcc7e3a8547ba168
millnitzluan <luanmillnitz@gmail.com>
2023-02-21 14:05:56 -0300

    udpdate plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     "git",
@@ -9,6 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
@@ -33,6 +35,7 @@ require("lazy").setup {
   -- Colorschemes
   "folke/tokyonight.nvim",
   "LunarVim/horizon.nvim",
+  "rebelot/kanagawa.nvim",
 
   -- cmp plugins
   "hrsh7th/nvim-cmp",
