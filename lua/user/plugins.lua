7a358e86e65e54d13ce18f5c0f016fd9996f8e6c
millnitzluan <luanmillnitz@gmail.com>
2023-03-30 13:01:28 -0300

    add windows plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -42,6 +42,21 @@ require("lazy").setup {
       }
     end,
   },
+
+  {
+    "anuvyklack/windows.nvim",
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      vim.o.winwidth = 10
+      vim.o.winminwidth = 10
+      vim.o.equalalways = false
+      require("windows").setup()
+    end,
+  },
+
   -- Colorschemes
   "folke/tokyonight.nvim",
   "LunarVim/horizon.nvim",
