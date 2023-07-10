ed851c464ba57f4926f5df2c9b2af8cc465545de
millnitzluan <luanmillnitz@gmail.com>
2023-07-10 12:09:50 -0300

    sort and update

--- lua/plugins/modules/nvim-treesitter.lua
+++ lua/plugins/modules/nvim-treesitter.lua
@@ -19,7 +19,7 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
          },
-        indent = { enable = true },
+        -- indent = { enable = true },
         context_commentstring = { enable = true, enable_autocmd = false },
         auto_install = true,
         ensure_installed = {
