20021243534c1d08593c4e8166c4ccb2c2ac8428
millnitzluan <luanmillnitz@gmail.com>
2023-08-21 11:42:54 -0300

    fix spellang

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -69,7 +69,7 @@ api.nvim_create_autocmd(
     pattern = { "*.txt", "*.md", "*.tex" },
     callback = function()
       vim.opt.spell = true
-      vim.opt.spelllang = "en,de"
+      vim.opt.spelllang = "en"
     end,
   }
 )
