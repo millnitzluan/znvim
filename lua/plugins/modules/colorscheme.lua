e9cc3af8d28c0dd494d8cd38f17c9148afd4c232
millnitzluan <luanmillnitz@gmail.com>
2024-04-26 13:20:28 -0300

    update

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -23,7 +23,7 @@ return {
       vim.api.nvim_set_hl(0, "NavicText", { default = true, bg = "none", fg = "#eedaad" })
       vim.api.nvim_set_hl(0, "NavicSeparator", { default = true, bg = "none", fg = "#eedaad" })
       --
-      vim.api.nvim_command("colorscheme catppuccin-macchiato")
+      vim.api.nvim_command("colorscheme catppuccin-mocha")
     end,
   },
   {
