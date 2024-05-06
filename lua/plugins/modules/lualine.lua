ea782a62cc2fee7be72426e99f6f1d79841bf889
millnitzluan <luanmillnitz@gmail.com>
2024-05-06 13:01:57 -0300

    update

--- lua/plugins/modules/lualine.lua
+++ lua/plugins/modules/lualine.lua
@@ -93,83 +93,8 @@ return {
             end,
           },
         },
-        lualine_y = {
-          { "location", icon = "", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
       },
       extensions = { "neo-tree", "lazy" },
     }
   end,
 }
--- return {
---   "nvim-lualine/lualine.nvim",
---   dependencies = {
---     "meuter/lualine-so-fancy.nvim",
---   },
---   enabled = true,
---   lazy = false,
---   event = { "BufReadPost", "BufNewFile", "VeryLazy" },
---   config = function()
---     require("lualine").setup({
---       options = {
---         theme = "auto",
---         globalstatus = true,
---         icons_enabled = true,
---         component_separators = { left = "|", right = "|" },
---         section_separators = { left = "", right = "" },
---         disabled_filetypes = {
---           statusline = {
---             "alfa-nvim",
---             "help",
---             "neo-tree",
---             "Trouble",
---             "spectre_panel",
---             "toggleterm",
---           },
---           winbar = {},
---         },
---       },
---       sections = {
---         lualine_a = {},
---         lualine_b = {
---           "fancy_branch",
---         },
---         lualine_c = {
---           {
---             "filename",
---             path = 1, -- 2 for full path
---             symbols = {
---               modified = "  ",
---               -- readonly = "  ",
---               -- unnamed = "  ",
---             },
---           },
---           { "fancy_diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " " } },
---           { "fancy_searchcount" },
---         },
---         lualine_x = {
---           "fancy_lsp_servers",
---           "fancy_diff",
---           "progress",
---         },
---         lualine_y = {},
---         lualine_z = {},
---       },
---       inactive_sections = {
---         lualine_a = {},
---         lualine_b = {},
---         lualine_c = { "filename" },
---         -- lualine_x = { "location" },
---         lualine_y = {},
---         lualine_z = {},
---       },
---       tabline = {},
---       extensions = { "neo-tree", "lazy" },
---     })
---   end,
--- }
