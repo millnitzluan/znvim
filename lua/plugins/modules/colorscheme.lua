78d7276c7d3be0d20e34acae4a635c45751646ee
millnitzluan <luanmillnitz@gmail.com>
2024-06-06 15:32:33 -0300

    update

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -378,9 +378,44 @@ return {
     end,
   },
   {
-    "tiagovla/tokyodark.nvim",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     enabled = true,
+    config = function()
+      -- Default options:
+      require("kanagawa").setup({
+        compile = false,  -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {             -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+
+      -- setup must be called before loading
+      vim.cmd("colorscheme kanagawa")
+    end,
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    enabled = false,
     opts = {},
     config = function(_, opts)
       require("tokyodark").setup(opts) -- calling setup is optional
--- lua/plugins/modules/lazy.lua
+++ lua/plugins/modules/lazy.lua
@@ -1,6 +1,6 @@
 return {
   "LazyVim/LazyVim",
   opts = {
-    colorscheme = "tokyodark",
+    colorscheme = "kanagawa",
   },
 }
