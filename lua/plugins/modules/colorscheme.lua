8504b7594c4173e9cf00fd9d7877dba21e206945
millnitzluan <luanmillnitz@gmail.com>
2024-06-17 11:23:34 -0300

    update

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -394,9 +394,14 @@ return {
         transparent = false,   -- do not set background color
         dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {             -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
         },
         overrides = function(colors) -- add/modify highlights
           return {}
